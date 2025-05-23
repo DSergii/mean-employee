0d1ea6aafd77c9a7d11b6aaf742b7a4982fa0673
Serhii <drozdsa@gmail.com>
2025-05-23 20:45:02 +0200

    upgrade Angular version from 17 to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -20,25 +20,18 @@ const routes = [
   { path: 'edit/:id', component: AddEmployeeComponent },
 ]
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    AddEmployeeComponent,
-    EmployeeListComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes),
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    HttpClientModule,
-    MatTableModule,
-    MatButtonModule,
-    MatInputModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        AddEmployeeComponent,
+        EmployeeListComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        RouterModule.forRoot(routes),
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatTableModule,
+        MatButtonModule,
+        MatInputModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
